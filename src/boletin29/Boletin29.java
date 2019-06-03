1ba5bd0be9de453e4178cfdc73f8217c7b143de8
clamascabaleiro <clamascabaleiro@nautilus15.danielcastelao.org>
2019-06-03 11:07:46 +0200

    Terminado

--- src/boletin29/Boletin29.java
+++ src/boletin29/Boletin29.java
@@ -10,7 +10,17 @@ public class Boletin29 {
 
  
     public static void main(String[] args) {
-       
+        Barco barco1 = new Veleros("AGSAGAS54",20.44f,3);
+        Barco barco2 = new Barco_Motor("KGLAGS5",5.62f,220);
+        Barco barco3 = new Yates("GIDSAS42",30.45f,7,5955);
+        barco1.lista.add(barco1);
+        barco2.lista.add(barco2);
+        barco3.lista.add(barco3);
+        
+        barco1.factura(0,5);
+        barco2.factura(0,5);
+        barco3.factura(0,5);
+
     }
 
 }
